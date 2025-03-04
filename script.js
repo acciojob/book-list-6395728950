35aa20ab077d58dd21957732fe620e3cddaa4221
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-03-04 10:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click",function(){
+	const t1 = document.getElementById("title").value.trim();
+	const a1 = document.getElementById("author").value.trim();
+	const i1 = document.getElementById("isbn").value.trim();
+	if(t1==="" || a1==="" || i1===""){
+		alert("please fill all the details");
+		return ;
+	}
+
+	const tablebody = document.querySelector("table tbody");
+	cosnt newrow = document.createElement(tr);
+	const first  = document.createElement(td);
+	const second  = document.createElement(td);
+	const last  = document.createElement(td);
+	first.textContent = t1;
+	second.textContent = a1;
+	last.textContent = i1;
+	newrow.appendChild(first);
+	newrow.appendChild(second);
+	newrow.appendChild(last);
+
+	tablebody.appendChild(newrow);
+	// t1.textContent.value="";
+	// a1.textContent.value="";
+	// i1.textContent.value="";
+	
+});
